72d0dd561a8ddc63fc7fa5280367ba284c6e419b
paramagician <konnor5456@gmail.com>
2018-11-19 02:51:59 +0000

    Updated routes & added new form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :user, only: %i[new create]
+  resources :users, only: %i[new create]
 end
