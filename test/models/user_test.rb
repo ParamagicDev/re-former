3aa8a1e691e377329b15aa68424cef0647c4296c
paramagician <konnor5456@gmail.com>
2018-11-19 01:51:37 +0000

    Added bcrypt, added validations, added tests for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(
+      name: 'konnor', email: 'konnor@example.com',
+      password: 'example', password_confirmation: 'example'
+    )
+  end
+
+  test 'user is valid' do
+    assert @user.valid?
+  end
+
+  test 'name must not be blank' do
+    @user.name = ' ' * 10
+    assert_not @user.valid?
+  end
+
+  test 'email must not be blank' do
+    @user.email = ' ' * 10
+    assert_not @user.valid?
+  end
+
+  test 'password cannot be blank' do
+    @user.password = ' ' * 10
+    @user.password_confirmation = ' ' * 10
+    assert_not @user.valid?
+  end
+
+  test 'password must equal password_confirmation' do
+    @user.password = 'Example'
+    assert_not @user.valid?
+  end
 end
+
