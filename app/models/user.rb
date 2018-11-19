74549febee9d0f43d87a4b983b0d808ae6e31364
paramagician <konnor5456@gmail.com>
2018-11-19 05:13:55 +0000

    Debugging why it does not show errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
+  validates :name, presence: true, length: { minimum: 3 }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: { maximum: 256 }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8, maximum: 50 }
+  validates :password_confirmation, presence: true, length: { minimum: 8, maximum: 50 }
 
   # will check for presence
   has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
-
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: %i[new create]
+
+  # Show must be added to view the edit url
+  resources :users, only: %i[new create edit update]
+
+  root 'users#index'
 end
+
